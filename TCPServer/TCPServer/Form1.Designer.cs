796f0e19ba875699e035eea3997cda1424a6b399
HiroyaHerdinanto <Herdinantohiroya@gmail.com>
2025-09-19 19:10:24 +0700

    first

---
+++ TCPServer/TCPServer/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿using System.Data.Common;
+
+namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblStatus = new Label();
+            lbConnections = new ListBox();
+            SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(28, 21);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(100, 15);
+            lblStatus.TabIndex = 0;
+            lblStatus.Text = "My IP Address is ...";
+            // 
+            // lbConnections
+            // 
+            lbConnections.FormattingEnabled = true;
+            lbConnections.ItemHeight = 15;
+            lbConnections.Location = new Point(28, 42);
+            lbConnections.Name = "lbConnections";
+            lbConnections.Size = new Size(359, 379);
+            lbConnections.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(425, 450);
+            Controls.Add(lbConnections);
+            Controls.Add(lblStatus);
+            Name = "Form1";
+            Text = "Server";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblStatus;
+        private ListBox lbConnections;
+    }
+}
